8fe595c4da166a9e4baa01f9f9346606edeb07b5
reshatamang <rejana43@gmail.com>
2019-02-04 16:25:23 +0545

    made backend

---
+++ resources/views/back/layouts/master.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+@include('back.layouts.head')
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+ @include('back.layouts.header')
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+@include('back.layouts.sidebar')
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dasbhboard
+      </h1>
+      <ol class="breadcrumb">
+        <li class="active"><i class="fa fa-dashboard"></i> Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+@yield('content')
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+@include('back.layouts.footer')
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 2.2.3 -->
+<script src="js/jquery-2.2.3.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="js/app.min.js"></script>
+@yield('script')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// front end
 
 Route::get('/', function () {
     return view('fronts.pages.index');
     // return view('fronts.layouts.master');
+});
+
+// Back end
+
+Route::get('/admin',function(){
+    return view('back.dashboard');
+    // return view('back.layouts.master');
 });
